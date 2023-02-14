621a2b6f815af8f0fbfbe64cc22fe2185a9fef9c
DivyaTalla01 <113543653+DivyaTalla01@users.noreply.github.com>
2023-02-14 16:15:54 -0600

    print statements and if condition
    print statements and if condition

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package project01;
-
+import java.util.*;
 /**
  *
  * @author
@@ -15,9 +15,29 @@ public class Project01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Carl Carver ");
-         System.out.println("Divya.Talla ");
         
-    }
+        
+        System.out.println("What would you like to do.");
+        System.out.println("Menu  (Choose one of the following or Q to quit:");
+        System.out.println("F - Filter on Type \n" +"D -Filter on Data\n"+ "S -Search by show \n"+"H -Highest days in top ten");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Choice:");
+        String name=input.nextLine();
+        
 
+        
+         if (name.equalsIgnoreCase("D")) {
+            System.out.print("What week would you like to search for (mm/dd/yyyy): ");
+            String date = input.nextLine();
+
+        
+        
+                 
+                 
+                 
+                 
+
+        }
+        
+}
 }
