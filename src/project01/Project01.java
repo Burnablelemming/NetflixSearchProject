f1d0c21305170bfc4a37381afb5c541e1c81d1ae
DivyaTalla01 <113543653+DivyaTalla01@users.noreply.github.com>
2023-02-20 13:24:27 -0600

    method
     Method that returns the data in a 2D array from file input

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -32,7 +32,6 @@ public class Project01 {
         while (input.hasNext()) {
 
         }
-
         //
         System.out.println("What would you like to do.");
         System.out.println("Menu  (Choose one of the following or Q to quit:");
@@ -43,29 +42,28 @@ public class Project01 {
 
     }
 
-    if (name.equalsIgnoreCase ( 
-        "D")) {
-            System.out.print("What week would you like to search for (mm/dd/yyyy): ");
-        String date = input.nextLine();
-
-    }
-
+  
+//method that returns the data in a 2D array from file input
     public static String[][] find(Scanner fileInput) throws FileNotFoundException {
         String[][] netflixData = new String[7100][6];
         //Choose the input File
         JFileChooser myChooser = new JFileChooser();
-
         myChooser.showOpenDialog(null);
-
         File file = myChooser.getSelectedFile();
 
         //Create Scanner
         Scanner input = new Scanner(file);
-
-        while (input.hasNext()) {
-            String date = input.next(",");
-            System.out.println(date);
+        int i=0;
+        while (fileInput.hasNextLine()) {
+            String line = fileInput.nextLine();
+            String[] fields = line.split(",");
+             for(int j=0;j<6;j++){
+                 netflixData[i][j]=fields[j];
+             }
+             i++;
         }
-
+        return netflixData;
+        
+    }  
     
 }
