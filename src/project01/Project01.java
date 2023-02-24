acb424c87fa939ae42807b31d56cbeef8dbf50de
DivyaTalla01 <113543653+DivyaTalla01@users.noreply.github.com>
2023-02-24 15:44:48 -0600

    trail

--- src/project01/Project01.java
+++ src/project01/Project01.java
@@ -1,30 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package project01;
 
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
 
-/**
- *
- * @author
- */
-public class Project01 {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Project01 {
     public static void main(String[] args) throws FileNotFoundException {
         //setting up user to choose file
         JFileChooser myChooser = new JFileChooser();
-
-        myChooser.showOpenDialog(null);
-
-        File file = myChooser.getSelectedFile();
+            myChooser.showOpenDialog(null);
+            File file = myChooser.getSelectedFile();
 
         //Create Scanner
         Scanner input = new Scanner(file);
@@ -32,17 +19,18 @@ public class Project01 {
         while (input.hasNext()) {
 
         }
-        //
+        //}
         System.out.println("What would you like to do.");
         System.out.println("Menu  (Choose one of the following or Q to quit:");
-        System.out.println("F - Filter on Type \n" + "D -Filter on Data\n" + "S -Search by show \n" + "H -Highest days in top ten");
+        System.out.println("F - Filter on Type \n" + "D -Filter on Data\n" + 
+                "S -Search by show \n" + "H -Highest days in top ten");
         input = new Scanner(System.in);
         System.out.print("Choice:");
         String name = input.nextLine();
 
     }
 
-  
+    
 //method that returns the data in a 2D array from file input
     public static String[][] find(Scanner fileInput) throws FileNotFoundException {
         String[][] netflixData = new String[7100][6];
